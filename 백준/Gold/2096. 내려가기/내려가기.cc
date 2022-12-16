323019220b446a32c138e260a0f64699e96f450d
younglee <lyoung0609@gmail.com>
2022-12-16 11:17:13 +0900

    [Gold V] Title: 내려가기, Time: 24 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Gold/2096. 내려가기/내려가기.cc
+++ 백준/Gold/2096. 내려가기/내려가기.cc
@@ -1,26 +1,21 @@
 /*
 -메모리최적화
-2차원 배열 -> 1차원 배열 재탕
+2차원 배열->1차원배열
 */
 
 #include <stdio.h>
 
 int N;
-int map[100000][3];
+int map[3];
 int max[3];
 int min[3];
 
 void init()
 {
-	scanf("%d", &N);
-	for (int i = 0; i < N; i++)
-		scanf("%d %d %d", &map[i][0], &map[i][1], &map[i][2]);
+	scanf("%d %d %d %d", &N, &map[0], &map[1], &map[2]);
 
 	for (int i = 0; i < 3; i++)
-	{
-		max[i] = map[0][i];
-		min[i] = map[0][i];
-	}
+		max[i] = min[i] = map[i];
 }
 
 int _max(int num1, int num2)
@@ -43,15 +38,17 @@ void solve()
 {
 	for (int i = 1; i < N; i++)
 	{
+		scanf("%d %d %d", &map[0], &map[1], &map[2]);
+
 		int max_term[3] = { max[0], max[1], max[2] };
-		max[0] = _max(max_term[0], max_term[1]) + map[i][0];
-		max[1] = _max(max_term[0], _max(max_term[1], max_term[2])) + map[i][1];
-		max[2] = _max(max_term[1], max_term[2]) + map[i][2];
+		max[0] = _max(max_term[0], max_term[1]) + map[0];
+		max[1] = _max(max_term[0], _max(max_term[1], max_term[2])) + map[1];
+		max[2] = _max(max_term[1], max_term[2]) + map[2];
 
 		int min_term[3] = { min[0], min[1], min[2] };
-		min[0] = _min(min_term[0], min_term[1]) + map[i][0];
-		min[1] = _min(min_term[0], _min(min_term[1], min_term[2])) + map[i][1];
-		min[2] = _min(min_term[1], min_term[2]) + map[i][2];
+		min[0] = _min(min_term[0], min_term[1]) + map[0];
+		min[1] = _min(min_term[0], _min(min_term[1], min_term[2])) + map[1];
+		min[2] = _min(min_term[1], min_term[2]) + map[2];
 	}
 }
 
